ae377b10c4cff8d15ea5b0b2c713a397148c2f35
Satvik Mishra <satvikmishra2192@gmail.com>
2025-10-01 12:44:31 +0530

    Project Initialised

---
+++ Saathi-Backend/src/main/java/com/hackathon/futurestack/saathibackend/SaathiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.futurestack.saathibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaathiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaathiBackendApplication.class, args);
+    }
+
+}
---
+++ Saathi-Backend/src/test/java/com/hackathon/futurestack/saathibackend/SaathiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.futurestack.saathibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaathiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
