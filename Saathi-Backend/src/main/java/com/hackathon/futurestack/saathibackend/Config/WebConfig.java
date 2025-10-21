d6a4e1dfb18185dba694f51526ca7c12f332ea8a
Satvik Mishra <satvikmishra2192@gmail.com>
2025-10-21 15:05:24 +0530

    Some more changes

--- Saathi-Backend/src/main/java/com/hackathon/futurestack/saathibackend/Config/WebConfig.java
+++ Saathi-Backend/src/main/java/com/hackathon/futurestack/saathibackend/Config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:5173/"
+                        "https://saathi-dusky.vercel.app/"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
